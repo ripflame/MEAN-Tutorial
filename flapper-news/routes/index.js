3d89cb2ad9ad0841bf518d5148988f7ac17ee23d
Gilberto Leon <ripflame@gmail.com>
2015-06-10 10:10:45 -0500

    Adding node

--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -1,9 +1,68 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+/* GET posts page. */
+router.get('/posts', function(req, res, next) {
+  Post.find(function(err, posts) {
+    if (err) {
+      return next(err);
+    }
+
+    res.json(posts);
+  });
+});
+
+/* POST new post */
+router.post('/posts', function(req, res, next) {
+  var post = new Post(req.body);
+
+  post.save(function(err, post) {
+    if (err) {
+      return next(err);
+    }
+
+    res.json(post);
+  });
+});
+
+/* Preload post objects */
+router.param('post', function(req, res, next, id) {
+  var query = Post.findById(id);
+
+  query.exec(function (err, post) {
+    if (err) {
+      return next(err);
+    }
+    if (!post) {
+      return next(new Error('Can\'t find post'));
+    }
+
+    req.post = post;
+    next();
+  });
+});
+
+router.get('/posts/:post', function(req, res){
+  res.json(req.post);
+});
+
+router.put('/posts/:post/upvote', function(req, res, next) {
+  req.post.upvote(function(err, post) {
+    if (err) {
+      return next(err);
+    }
+
+    res.json(post);
+  });
+});
+
 
 module.exports = router;
