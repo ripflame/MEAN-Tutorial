f2431dcaad470dbaebec03fd5d29d38ef500f013
Gilberto Leon <ripflame@gmail.com>
2015-06-10 10:58:35 -0500

    Fixing routing on app.js

--- flapper-news/app.js
+++ flapper-news/app.js
@@ -4,17 +4,17 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var mongoose = require('mongoose');
+
+var app = express();
+
+mongoose.connect('mongodb://localhost/news');
+
 require('./models/Posts');
 require('./models/Comments');
-mongoose.connect('mongodb://localhost/news');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
-
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
